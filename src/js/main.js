47a35e231285dc7636c8e25a575bc3b314b65c9e
Bruno Donadei <bruno.donadei@hiplatform.com>
2025-04-04 00:44:27 -0300

    refactor: change all logics and appearence of the project

--- src/js/main.js
+++ src/js/main.js
@@ -1,72 +1,69 @@
-'use strict';
-const apareceForms = () => {
-    document.getElementById('forms').style.display = 'block'
-}
+"use strict";
+const isNumber = (numero) => /^[0-9]+$/.test(numero);
+const isValidCep = (cep) => cep.length == 8 && isNumber(cep);
+const cep = document.getElementById("cep");
+const loading = document.getElementById("loading");
+const form = document.getElementById("forms");
 
-const limparForms = () => {
-    document.getElementById('forms').style.display = 'none'
-}
+const fields = [
+  "logradouro",
+  "complemento",
+  "bairro",
+  "localidade",
+  "uf",
+  "ibge",
+  "gia",
+  "ddd",
+  "siafi",
+];
+const setValue = (id, value) => (document.getElementById(id).value = value);
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const fillForm = (event) => {
+  fields.forEach((f) => {
+    setValue(f, event[f]);
+  });
+};
 
-const showError = () => {
-    document.getElementById('error').style.display = 'block'
-}
+const getCep = async () => {
+  loading.style.display = "inline-block";
+  const formattedCep = cep.value.replace("-", "");
+  const url = `https://viacep.com.br/ws/${formattedCep}/json/`;
+  const errorMessage = document.getElementById("error");
 
-const showError2 = () => {
-    document.getElementById('error2').style.display = 'block'
-}
+  if (isValidCep(formattedCep)) {
+    try {
+      const fetchCep = fetch(url).then((res) => res.json());
+      const minDelay = new Promise((resolve) => setTimeout(resolve, 500));
+      const [response] = await Promise.all([fetchCep, minDelay]);
 
-const limpaErros = () => {
-    document.getElementById('error').style.display = 'none'
-    document.getElementById('error2').style.display = 'none'
-}
-
-
-document.getElementById('forms').style.display = 'none'
-
-const preencherFormulario = (event) =>{
-    document.getElementById('logradouro').value = event.logradouro
-    document.getElementById('complemento').value = event.complemento
-    document.getElementById('bairro').value = event.bairro
-    document.getElementById('localidade').value = event.localidade
-    document.getElementById('uf').value = event.uf
-    document.getElementById('ibge').value = event.ibge
-    document.getElementById('gia').value = event.gia
-    document.getElementById('ddd').value = event.ddd
-    document.getElementById('siafi').value = event.siafi
-}
-
-document.getElementById('error').style.display = 'none'
-document.getElementById('error2').style.display = 'none'
-
-
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
-
-const cepValido = (cep) => cep.length == 8 && eNumero(cep); 
-
-const pesquisarCep = async() => {
-    limparForms();
-    
-    const cep = document.getElementById('cep').value.replace("-","");
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
-    if (cepValido(cep)){
-        const dados = await fetch(url);
-        const endereco = await dados.json();
-        if (endereco.hasOwnProperty('erro')){
-            limparForms()
-            limpaErros()
-            showError2()
-        }else {
-            preencherFormulario(endereco);
-            apareceForms()
-            limpaErros()
-        }
-    }else{
-        limparForms()
-        limpaErros()
-        showError()
+      if (response.erro) {
+        errorMessage.innerHTML = "CEP não existe.";
+        form.style.display = "none";
+        console.log(form.style.display);
+        return null;
+      }
+      form.style.display = "block";
+      errorMessage.innerHTML = "";
+      return { response };
+    } catch (error) {
+      errorMessage.innerHTML = error;
+      loading.style.display = "none";
+      form.style.display = "none";
+      return null;
+    } finally {
+      loading.style.display = "none";
     }
-     
-}
+  }
+  errorMessage.innerHTML = "CEP inválido.";
+  form.style.display = "none";
+  loading.style.display = "none";
+  return null;
+};
 
-document.getElementById('cep')
-        .addEventListener('focusout',pesquisarCep);
+const handleSubmit = async () => {
+  const { response } = await getCep();
+  if (!response) return;
+  fillForm(response);
+};
+
+cep.addEventListener("focusout", handleSubmit);
